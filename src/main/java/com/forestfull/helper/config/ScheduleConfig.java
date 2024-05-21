1a2763c804627f8deeed97262c5beea42c6437d0
HyeonSeok Ko <bigfoot7774@gmail.com>
2024-05-21 16:00:31 +0900

    mariadb, jasypt  기본연동

---
+++ src/main/java/com/forestfull/helper/config/ScheduleConfig.java
@@ -0,0 +1,12 @@
+package com.forestfull.helper.config;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class ScheduleConfig {
+
+
+
+}
---
+++ src/main/java/com/forestfull/helper/config/UtilsConfig.java
@@ -0,0 +1,42 @@
+package com.forestfull.helper.config;
+
+import jakarta.annotation.PostConstruct;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.function.BiConsumer;
+
+@Configuration
+public class UtilsConfig {
+
+    private static class EncryptionSetting {
+        public static void main(String[] args) {
+
+        }
+
+        private static StringEncryptor initEncryptor(String key) {
+            final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+            encryptor.setPassword(key);
+            encryptor.setKeyObtentionIterations(1000);
+            encryptor.setPoolSize(1);
+            return encryptor;
+        }
+
+        private static final BiConsumer<String, String> enc = (key, str)
+                -> System.out.println(str + " : ENC(" + initEncryptor(key).encrypt(str) + ")");
+        private static final BiConsumer<String, String> dec = (key, str)
+                -> System.out.println(str + " : " + initEncryptor(key).decrypt(str));
+    }
+
+    @Value("${spring.profiles.active}")
+    private String key;
+
+    @Bean("jasyptStringEncryptor")
+    StringEncryptor stringEncryptor() {
+        return EncryptionSetting.initEncryptor(key);
+    }
+}
