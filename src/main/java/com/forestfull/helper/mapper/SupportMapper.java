4b34e296ab0e8d1d85f69449a76845c903c7069f
HyeonSeok Ko <bigfoot7774@gmail.com>
2024-05-24 11:41:39 +0900

    심플하게 변경하고 이제 구현 시작

--- src/main/java/com/forestfull/helper/mapper/SupportMapper.java
+++ src/main/java/com/forestfull/helper/mapper/SupportMapper.java
@@ -3,25 +3,18 @@ package com.forestfull.helper.mapper;
 import com.forestfull.helper.domain.Client;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.jdbc.SQL;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface SupportMapper {
 
-    @SelectProvider(type = Provider.class, method = "getHistoriesByClientToken")
+    @Select("SELECT ch.client_id, ch.type, ch.ip_address, ch.data, ch.created_time FROM client_history ch JOIN client c ON ch.client_id = c.id AND c.token = #{token}")
     List<Client.History> getHistoriesByClientToken(@Param("token") String token);
 
-    class Provider {
-        public static String getHistoriesByClientToken(@Param("token") String token) {
-            return new SQL() {{
-                SELECT("ch.client_id, ch.type, ch.ip_address, ch.data, ch.created_time");
-                FROM("client_history ch");
-                JOIN("client c ON ch.client_id = c.id AND c.token = #{token}");
 
-            }}.toString();
-        }
+    class Provider {
+
     }
 }
